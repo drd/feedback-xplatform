0c6ff786cab9b0143387339b9a1001cace6b14f3
Eric O'Connell <eric@premise.com>
2018-07-02 15:53:05 -0700

    Initial Commit

---
+++ fb macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  fb macOS
+//
+//  Created by Eric O'Connell on 7/2/18.
+//  Copyright © 2018 compassing. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
